8a3e7e8df3f06cd496b03b2d835463ae91c1adfc
Jai Gautam <jaigautam1405@gmail.com>
2023-02-25 01:14:26 +0000

    Updated logger to no longer use busy-waiting, avoiding performance penalties.

--- Utilities/ParallelLogger.cs
+++ Utilities/ParallelLogger.cs
@@ -1,63 +1,58 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AemulusModManager.Utilities
 {
     public static class ParallelLogger
     {
+        private const int FlushInterval = 100;
         private static readonly BlockingCollection<string> _Queue = new BlockingCollection<string>();
+        private static readonly object _QueueLock = new object();
+        private static bool _IsFlushing = false;
+        private static readonly Timer _FlushTimer = new Timer(FlushLogMessages, null, FlushInterval, FlushInterval);
 
-        static ParallelLogger()
+        private static void FlushLogMessages(object state)
         {
-            var thread = new Thread(
-              () =>
-              {
-                  StringBuilder builder = new StringBuilder();
-                  int count = 0;
-                  Stopwatch sw = Stopwatch.StartNew();
-                  sw.Start();
-                  while (true)
-                  {
-                      count = _Queue.Count;
-                      if (count > 0)
-                      {
-                          if (sw.ElapsedMilliseconds > 100)
-                          {
-                              sw.Restart();
-                              for (int i = 0; i < count; i++)
-                              {
-                                  builder.AppendLine(_Queue.Take());
-                              }
-                              Console.WriteLine(builder.ToString());
-                              builder.Clear();
-                          }
-                      }
+            if (_IsFlushing) return;
+            lock (_QueueLock)
+            {
+                if (_IsFlushing || _Queue.Count == 0) return;
+                _IsFlushing = true;
+            }
 
+            StringBuilder builder = new StringBuilder();
+            int count = _Queue.Count;
+            for (int i = 0; i < count; i++)
+            {
+                builder.AppendLine(_Queue.Take());
+            }
+            Console.WriteLine(builder.ToString());
+            builder.Clear();
 
-                  }
-              });
-            thread.IsBackground = true;
-            thread.Start();
+            lock (_QueueLock)
+            {
+                _IsFlushing = false;
+            }
         }
 
         [Conditional("DEBUG")]
-        public static void AddDebugInfo(string caller, int line, ref string message) {
-            if(line != -1) message =  $"({caller}-{line}){message}";
+        public static void AddDebugInfo(string caller, int line, ref string message)
+        {
+            if (line != -1) message = $"({caller}-{line}){message}
         }
-        
-        public static void Log(string message, 
+
+        public static void Log(string message,
             [System.Runtime.CompilerServices.CallerMemberName] string caller = "",
             [System.Runtime.CompilerServices.CallerLineNumber] int line = -1)
-        { 
+        {
             AddDebugInfo(caller, line, ref message);
-            _Queue.Add(message);
+            lock (_QueueLock)
+            {
+                _Queue.Add(message);
+            }
         }
     }
-
 }
