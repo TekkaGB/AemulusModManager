7247c3d78913d3a2a38f09740790e0610d10a491
oceanstuck <56813824+oceanstuck@users.noreply.github.com>
2024-06-29 00:47:53 -0700

    merge q1-support

--- Utilities/TblPatch.cs
+++ Utilities/TblPatch.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Reflection;
-using AtlusScriptLibrary.Common.Collections;
 
 namespace AemulusModManager
 {
